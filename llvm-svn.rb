5ee5d512004e2c7c4d5d92fc93a98213db1486bc
rhysd <lin90162@yahoo.co.jp>
2013-08-24 21:56:15 +0900

    use man1 helper

--- llvm-svn.rb
+++ llvm-svn.rb
@@ -121,10 +121,10 @@ class LlvmSvn < Formula
     end
 
     # Also link man pages
-    mkdir_p share/'man/man1'
+    mkdir_p man1
     Dir.glob(install_prefix/'share/man/man1/*') do |manpage|
       manpage_base = File.basename(manpage, '.1')
-      ln_s manpage, share/"man/man1/#{manpage_base}-#{suffix}.1"
+      ln_s manpage, man1/"#{manpage_base}-#{suffix}.1"
     end
   end
 
