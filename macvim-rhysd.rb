eb7571d4d17620e2321e15fa93809c13b75484f6
rhysd <lin90162@yahoo.co.jp>
2013-12-08 00:54:09 +0900

    add my patch version MacVim

---
+++ macvim-rhysd.rb
@@ -0,0 +1,187 @@
+require 'formula'
+
+# Reference: https://github.com/b4winckler/macvim/wiki/building
+class MacvimRhysd < Formula
+  homepage 'http://code.google.com/p/macvim/'
+  url 'https://github.com/b4winckler/macvim/archive/snapshot-72.tar.gz'
+  version '7.4-72'
+  sha1 '3fb5b09d7496c8031a40e7a73374424ef6c81166'
+
+  head 'https://github.com/b4winckler/macvim.git', :branch => 'master'
+
+  option "custom-icons", "Try to generate custom document icons"
+  option "override-system-vim", "Override system vim"
+
+  depends_on :xcode
+  depends_on 'cscope' => :recommended
+  depends_on 'lua' => :optional
+  depends_on 'luajit' => :optional
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
+
+  env :std if MacOS.version <= :snow_leopard
+  # Help us! We'd like to use superenv in these environments too
+
+  conflicts_with 'macvim',
+    :because => 'This formula installs my patchedversion of MacVim.'
+
+  def patches
+    {
+      :p1 => 'https://bitbucket.org/k_takata/vim-ktakata-mq/raw/98482edd59b30091f30371dcadad4e3ffcc132be/vim-7.4.035-breakindent.patch',
+      :p1 => 'https://bitbucket.org/koron/vim-kaoriya-patches/raw/6658116d59073a4471a83fea41a0791718773a96/X010-autoload_cache.diff',
+      :p1 => 'https://gist.github.com/Shougo/5654189/raw',
+    }
+  end
+
+  def install
+    # MacVim doesn't have and required any Python package, unset PYTHONPATH.
+    ENV.delete('PYTHONPATH')
+
+    # Set ARCHFLAGS so the Python app (with C extension) that is
+    # used to create the custom icons will not try to compile in
+    # PPC support (which isn't needed in Homebrew-supported systems.)
+    ENV['ARCHFLAGS'] = "-arch #{MacOS.preferred_arch}"
+
+    # If building for 10.7 or up, make sure that CC is set to "clang".
+    ENV.clang if MacOS.version >= :lion
+
+    # macvim HEAD only works with the current Ruby.framework because it builds with -framework Ruby
+    system_ruby = "/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby"
+
+    args = %W[
+      --with-features=huge
+      --enable-multibyte
+      --with-macarchs=#{MacOS.preferred_arch}
+      --enable-perlinterp
+      --enable-rubyinterp
+      --enable-tclinterp
+      --with-ruby-command=#{system_ruby}
+      --with-tlib=ncurses
+      --with-compiledby=Homebrew
+      --with-local-dir=#{HOMEBREW_PREFIX}
+    ]
+
+    args << "--with-macsdk=#{MacOS.version}" unless MacOS::CLT.installed?
+    args << "--enable-cscope" if build.with? "cscope"
+
+    if build.with? "lua"
+      args << "--enable-luainterp"
+      args << "--with-lua-prefix=#{HOMEBREW_PREFIX}"
+    end
+
+    if build.with? "luajit"
+      args << "--enable-luainterp"
+      args << "--with-lua-prefix=#{HOMEBREW_PREFIX}"
+      args << "--with-luajit"
+    end
+
+    if build.with? "python" and build.with? "python3"
+      args << "--enable-pythoninterp=dynamic" << "--enable-python3interp=dynamic"
+    else
+      args << "--enable-pythoninterp" if build.with? "python"
+      args << "--enable-python3interp" if build.with? "python3"
+    end
+
+    # MacVim seems to link Python by `-framework Python` (instead of
+    # `python-config --ldflags`) and so we have to pass the -F to point to
+    # where the Python.framework is located, we want it to use!
+    # Also the -L is needed for the correct linking. This is a mess but we have
+    # to wait until MacVim is really able to link against different Python's
+    # on the Mac. Note configure detects brewed python correctly, but that
+    # is ignored.
+    # See https://github.com/mxcl/homebrew/issues/17908
+    if python2 and python2.framework? and not build.with? "python3"
+      ENV.prepend 'LDFLAGS', "-L#{python2.libdir} -F#{python2.framework}"
+    end
+
+    unless MacOS::CLT.installed?
+      # On Xcode-only systems:
+      # Macvim cannot deal with "/Applications/Xcode.app/Contents/Developer" as
+      # it is returned by `xcode-select -print-path` and already set by
+      # Homebrew (in superenv). Instead Macvim needs the deeper dir to directly
+      # append "SDKs/...".
+      args << "--with-developer-dir=#{MacOS::Xcode.prefix}/Platforms/MacOSX.platform/Developer/"
+    end
+
+    system "./configure", *args
+
+    if build.with? "python" and build.with? "python3"
+      # On 64-bit systems, we need to avoid a 32-bit Framework Python.
+      # MacVim doesn't check Python while compiling.
+      python do
+        if MacOS.prefer_64_bit? and python.framework? and not archs_for_command("#{python.prefix}/Python").include? :x86_64
+          opoo "Detected a framework Python that does not have 64-bit support built in:"
+          puts <<-EOS.undent
+            #{python.prefix}
+
+            Dynamic loading Python library requires the same architecture.
+
+            Note that a framework Python in /Library/Frameworks/Python.framework is
+            the "MacPython" version, and not the system-provided version which is in:
+              /System/Library/Frameworks/Python.framework
+          EOS
+        end
+      end
+      # Help vim find Python's library as absolute path.
+      python do
+        inreplace 'src/auto/config.mk', /-DDYNAMIC_PYTHON#{python.if3then3}_DLL=\\".*\\"/, %Q[-DDYNAMIC_PYTHON#{python.if3then3}_DLL=\'\"#{python.prefix}/Python\"\'] if python.framework?
+      end
+      # Force vim loading different Python on same time, may cause vim crash.
+      unless python.brewed?
+        opoo "You seem to have a version of Python not installed by Homebrew. Attempting to modify config.h. You may see warning message during brewing."
+        inreplace 'src/auto/config.h', "/* #undef PY_NO_RTLD_GLOBAL */", "#define PY_NO_RTLD_GLOBAL 1"
+        inreplace 'src/auto/config.h', "/* #undef PY3_NO_RTLD_GLOBAL */", "#define PY3_NO_RTLD_GLOBAL 1"
+      end
+    end
+
+    if build.include? "custom-icons"
+      # Get the custom font used by the icons
+      cd 'src/MacVim/icons' do
+        system "make getenvy"
+      end
+    else
+      # Building custom icons fails for many users, so off by default.
+      inreplace "src/MacVim/icons/Makefile", "$(MAKE) -C makeicns", ""
+      inreplace "src/MacVim/icons/make_icons.py", "dont_create = False", "dont_create = True"
+    end
+
+    system "make"
+
+    prefix.install "src/MacVim/build/Release/MacVim.app"
+    inreplace "src/MacVim/mvim", /^# VIM_APP_DIR=\/Applications$/,
+                                 "VIM_APP_DIR=#{prefix}"
+    bin.install "src/MacVim/mvim"
+
+    # Create MacVim vimdiff, view, ex equivalents
+    executables = %w[mvimdiff mview mvimex gvim gvimdiff gview gvimex]
+    executables += %w[vi vim vimdiff view vimex] if build.include? "override-system-vim"
+    executables.each {|f| ln_s bin+'mvim', bin+f}
+  end
+
+  def caveats
+    s = ''
+    s += <<-EOS.undent
+      MacVim.app installed to:
+        #{prefix}
+
+      Run the following to make MacVim available as a Mac OS X application:
+          brew linkapps
+      or:
+          brew linkapps --local
+    EOS
+    if build.with? "python" and build.with? "python3"
+      s += <<-EOS.undent
+
+        MacVim has been built with dynamic loading of Python 2 and Python 3.
+
+        Note: if MacVim dynamically loads both Python 2 and Python 3, it may
+        crash. For more information, see:
+            http://vimdoc.sourceforge.net/htmldoc/if_pyth.html#python3
+      EOS
+    end
+    return s
+  end
+end
+
+
+
